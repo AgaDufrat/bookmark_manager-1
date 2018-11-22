81b4f28b8e4580c38a0c3088ee1e0dc9eac7f3f0
Will Wayman <will.jdw@gmail.com>
2018-11-22 16:29:23 +0000

    feature test for checking valid url

--- spec/features/creating_bookmark_spec.rb
+++ spec/features/creating_bookmark_spec.rb
@@ -7,4 +7,14 @@ feature 'Adding a new bookmark' do
 
     expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
   end
+  
+  scenario 'the bookmark must be a valid url' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'not a bookmark')
+    fill_in('title', with: 'Test Bookmark')
+    click_button('Submit')
+
+    expect(page).not_to have_content('not a bookmark')
+    expect(page).to have_content('You must submit a valid URL.')
+  end
 end
